de8f2bcd9ab2ca12eab97c43dab65c7e79541448
MNBrytowski <mn.brytowski@gmail.com>
2018-11-11 16:33:49 -0700

    more updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,10 +3,11 @@
 //Ajax call to pull gifs
 //Create new buttons by user input, loop
 $(document).ready(function () {
-    var list = ["Friends", "Parks and Recreation", "Brooklyn 99", "Seinfeld", "Psych"];
-    console.log(list[1])
+    var topics = ["Friends", "Parks and Recreation", "Brooklyn 99", "Seinfeld", "Psych", "New Girl"];
+    console.log(topics[1])
     //GIF function
-    $(document).on("click", ".showBtn", function() {
+    $(document).on("click", ".showBtn", function () {
+        $("#shows-view").empty();
         console.log('Click');
         var show = $(this).attr("data-label");
         var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + show + "&api_key=33y1uwrgHHUHfqtkDlYEd12S1OPdqCqE&limit=10";
@@ -27,37 +28,79 @@ $(document).ready(function () {
                     var rating = results[i].rating;
                     //create p tag
                     var p = $("<p>").text("Rating: " + rating);
-                    //Append rating to html
-                    showDiv.append(p);
-                    // var imgStill = results[i].images.fixed_height_still;
-
                     var image = $("<img>");
+                    image.addClass("gif");
                     image.attr("src", results[i].images.fixed_height_still.url);
+                    image.attr("data-still", results[i].images.fixed_height_still.url);
+                    image.attr("data-animate", results[i].images.fixed_height.url);
+                    image.attr("data-state", results[i].images.fixed_height_still.url);
+
+                    //Append to html
+
                     showDiv.append(image);
+                    showDiv.append(p);
+
+
                     $("#shows-view").prepend(showDiv);
+
                 }
-            }
+            };
+
+            $(".gif").on("click", function () {
+                var src = $(this).attr("data-animate");
+                $(this).attr("src", src);
+            // },
+            // function(){
+            //     var src = $(this).attr("data-still");
+            //     $(this).attr("src", src);
+            })
+
+            // for (var i = 0; i < animatedGIFArray.length; i++) {
+            //     var gifAnimated = $("<img>");
+            //     gifAnimated.attr("src", results[i].images.original.url);
+            //     gifAnimated.addClass("animated");
+            //     gifAnimated.attr("data-animated");
+
+            //     animatedGIFArray.push(gifAnimated);
+            // }
 
         });
 
+
     });
+
+
+
 
 
     //Button loop
     function renderButtons() {
         $("#buttonPlace").empty();
 
-        for (var i = 0; i < list.length; i++) {
+        for (var i = 0; i < topics.length; i++) {
             var y = $("<button>");
             y.addClass("showBtn");
-            y.attr("data-label", list[i]);
-            y.text(list[i]);
+            y.attr("data-label", topics[i]);
+            y.text(topics[i]);
             $("#buttonPlace").append(y);
+
         }
+        $("#tv-input").empty();
     }
 
-    // $(document).on("click", ".tvShow", displayGIF);
+    $("#add-tvShow").on("click", function (event) {
+        event.preventDefault();
+
+        var tvShow = $("#tv-input").val().trim();
+
+        topics.push(tvShow);
+
+        renderButtons();
+
+    });
+
 
     renderButtons();
+
 
 })
